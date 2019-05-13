f3bacf42432517e3ffc2d4ea7d4b843916c0875a
Carlos Diego <diegolima@aptaretecnologia.com.br>
2019-05-13 18:25:26 +0000

    trabalhador.service.ts edited online with Bitbucket

--- src/app/services/trabalhador/trabalhador.service.ts
+++ src/app/services/trabalhador/trabalhador.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
 import { AptareCrudService } from '../shared/aptare-crud.service';
 import { Trabalhador } from '../../model/trabalhador/trabalhador';
+import { Espaco } from '../../model/espaco/espaco';
+import { HOST_CEFIT } from '../cefit.api';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class TrabalhadorService extends AptareCrudService<Trabalhador> {
 
-    public static SITUACAO_PENDENTE = 1;
-    public static SITUACAO_ATIVA = 2;
-    public static SITUACAO_INATIVA = 3;
+  public static SITUACAO_PENDENTE = 1;
+  public static SITUACAO_ATIVA = 2;
+  public static SITUACAO_INATIVA = 3;
 
-    artefato(): string {    
-        return "trabalhador";
-    }
+  constructor(public http: HttpClient) {
+    super(http);
+  }
+
+  salvarManutencao(obj: Trabalhador) {
+    return this.http.post(`${HOST_CEFIT}/api/` + this.artefato() + `/salvarManutencao`, obj);
+  }
+
+  artefato(): string {
+    return 'trabalhador';
+  }
 
 }
